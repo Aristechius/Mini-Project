40eb20ed21d38fdd138040f4080dfef8dbbd7dcf
aristechius <aris.karumuna@gmail.com>
2015-07-20 23:13:28 +0300

    created the students and staffs classes also added their member variables

---
+++ src/my/AttendanceCalculator/staffs.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.AttendanceCalculator;
+
+/**
+ *
+ * @author aristechius
+ */
+public class staffs {
+    String firstName,lastName;
+    Float ID;
+    
+}
