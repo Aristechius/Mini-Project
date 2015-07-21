1e75ea0fa6fe9a57832213237dc300235b6066a3
Francis Sowani <frathoso@gmail.com>
2015-07-21 23:52:42 +0400

    Add setters and getters

--- src/my/AttendanceCalculator/Staff.java
+++ src/my/AttendanceCalculator/Staff.java
@@ -10,7 +10,41 @@ package my.AttendanceCalculator;
  * @author aristechius
  */
 public class Staff {
-    String firstName,lastName;
-    Float ID;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private int  id;
     
+    /*
+     * Constructor
+     */
+    public Staff(){
+        firstName = null;
+        lastName = null;
+        password = null;
+        id = -1;
+    }
+    
+    public Staff(int id, String firstName, String lastName, String password){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.id = id;
+    }
+    
+    /*
+     * Getters
+     */
+    public String getFirstName(){ return this.firstName; }
+    public String getLastName(){ return this.lastName; }
+    public String getPassword(){ return this.password; }
+    public int getID(){ return this.id; }
+    
+    /*
+     * Setters
+     */
+    public void setFirstName(String firstName){ this.firstName = firstName; }
+    public void setLastName(String lastName){ this.lastName = lastName; }
+    public void setPassword(String password){ this.password = password; }
+    public void setID(int id){ this.id = id; }
 }
